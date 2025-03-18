87a3b8e9cc64d509b6d51d65d67bfeb7736078ec
Arif <aarifokcan@gmail.com>
2025-03-18 07:21:41 +0300

    Modify too many things. However, it's still doesn't work

--- src/main.c
+++ src/main.c
@@ -12,65 +12,107 @@
 
 #include "../lib/libft/libft.h"
 #include "../inc/push_swap.h"
+#include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-void	init_stacks(t_stacks *stacks)
+/*
+ * NOOOT NOOOOOOOOT! WE HAVE A SEGMENTATION FAULT HERE!!!!!!!
+ *
+ */
+
+
+void	init_stacks(t_stacks *stacks, int size)
 {
-	stacks->a = NULL;
-	stacks->b = NULL;
-	stacks->a_size = 0;
-	stacks->a_size = 0;
-}
-
-int	insert_atoi()
-{
-}
-
-void	free_imp(char **str)
-{
-	int	i;
-
-	if (!*str)
-		return ;
-	i = 0;
-	while (str[i])
+	stacks->a = malloc(sizeof(int) * size);
+	if (!stacks->a)
+		exit (1);
+	stacks->b = malloc(sizeof(int) * size);
+	if (!stacks->b)
 	{
-		free(str[i]);
+		free (stacks->a);
+		exit (1);
+	}
+	stacks->a_size = 0;
+	stacks->b_size = 0;
+}
+
+void	handle_string_arg(char *arg, t_stacks *stack)
+{
+	char			**split_buffer;
+	unsigned int	i;
+
+	split_buffer = ft_split(arg, ' ');
+	if (!split_buffer)
+		exit (1);
+	i = 0;
+	while (split_buffer[i])
+	{
+		stack->a[stack->a_size] = insert_atoi(split_buffer[i], stack);
+		stack->a_size++;
 		i++;
 	}
-	free(str);
 }
 
-void	insert_stacks(int argc, char **arg, t_stacks stack)
+void	insert_stacks(int argc, char **arg, t_stacks *stack)
 {
-	char	**split_buffer;
 	int		i;
-	int		j;
 
 	i = 1;
-	j = 0;
 	while (i < argc)
 	{
 		if (ft_strchr(arg[i], ' '))
 		{
-			while (1)
-			{
-				split_buffer = ft_split(arg[i], ' ');
-				if (!split_buffer)
-					break ;
-
-			}
+			//write(1, "A\n", 2);
+			handle_string_arg(arg[i], stack);
 		}
 		else
 		{
-			stack.a[stack.a_size] = insert_atoi();
-			stack.a_size++;
+			stack->a[stack->a_size] = insert_atoi(arg[i], stack);
+			if (stack->a[stack->a_size] == -1)
+				free_stacks(stack);
+			stack->a_size++;
 		}
 		i++;
 	}
 }
 
-// DONT FORGET TO MALLOC STACKS!!!!
+int	arg_size(char *arg)
+{
+	char	**split_buffer;
+	int		i;
+
+	i = 0;
+	split_buffer = ft_split(arg, ' ');
+	if (!split_buffer)
+		exit(1);
+	while (split_buffer[i])
+		i++;
+	free_imp(split_buffer);
+	return (i);
+}
+
+int	stack_size(char **arg)
+{
+	unsigned int	i;
+	int				stack_size;
+
+	stack_size = 0;
+	i = 1;
+	while (arg[i])
+	{
+		if (arg[i][0] == '\0')
+			exit(1);
+		if (ft_strchr(arg[i], ' '))
+			stack_size += arg_size(arg[i]);
+		else
+			stack_size++;
+		i++;
+	}
+	printf("TOTAL: %d\n", stack_size);
+	return (stack_size);
+}
 
 int	main(int argc, char **argv)
 {
@@ -78,6 +120,6 @@ int	main(int argc, char **argv)
 
 	if (argc < 2)
 		exit(1);
-	init_stacks(&stack);
-	insert_stacks(argc, &argv[1], stack);
+	init_stacks(&stack, stack_size(argv));
+	insert_stacks(argc, &argv[1], &stack);
 }
